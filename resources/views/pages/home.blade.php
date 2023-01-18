936e2f026b4d140f580906f2667df61cdc3763be
NS <anhson231557@gmail.com>
2023-01-18 22:25:47 +0700

    Tetst Git tren hosting vietnix

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,6 +5,7 @@
         @include('pages.include.homepages.slider')
         <!--main-->
         <main id="main-contents" class="col-xs-12 col-sm-12 col-md-8">
+            <h1>lkdfsjlfadskj</h1>
             @foreach($categories as $key=>$category)
                 <section id="halim-advanced-widget-2">
                     <div class="section-heading">
